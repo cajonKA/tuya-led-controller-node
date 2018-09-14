233dde557b3a116b7f73fd38eae0ea23cc040e4d
cajonKA <>
2018-09-14 22:54:06 +0200

    -added IP Discovery if IP is set to 0.0.0.0 -added node connect status

--- magichome-led.js
+++ magichome-led.js
@@ -1,5 +1,5 @@
 var color = require('./color/color');
-module.exports = function(RED) {
+module.exports = function (RED) {
 
     function MagicHomeLedNode(config) {
         RED.nodes.createNode(this, config);
@@ -8,12 +8,16 @@ module.exports = function(RED) {
         this.deviceid = config.deviceid;
         this.key = config.key;
         this.ip = config.ip;
+
+
         //and apply config to object
         var tuyacfg = {
             id: this.deviceid,
             key: this.key,
             ip: this.ip
         };
+        
+
         var node = this;
 
         //link tuya api
@@ -22,37 +26,83 @@ module.exports = function(RED) {
         tuya = new TuyaDevice(tuyacfg);
         console.log("MagicHome Led Node Created");
 
-        node.on('close', function() {
+        // function to try to discover the controllers IP
+        function discoverIP() {
+            tuya.resolveId()
+                .then(status => {
+                    console.log("Magichome Led Controller IP discovered as : " + tuya.device.ip);
+                    node.status({ fill: "green", shape: "dot", text: "connected" });
+                })
+                .catch(error => {
+                    console.log("Controller not found: " + error);
+                    node.status({ fill: "red", shape: "ring", text: "no connection" });
+                });
+        };
+
+        // IF IP is zero, try to detect it.
+        if (tuya.device.ip == "0.0.0.0") {
+            delete tuya.device.ip;
+            discoverIP();
+        }
+
+        function getStatus() {
+            tuya.get()
+                .then(status => node.status({ fill: "green", shape: "dot", text: "connected" }))
+                // if getting status failes, try to newly discover IP
+                .catch(error => {
+                    node.status({ fill: "red", shape: "ring", text: "no connection" });
+                    delete tuya.device.ip;
+                    discoverIP()
+                });
+        }
+
+        getStatus();
+
+        setInterval(function () {
+            getStatus()
+        }, 60000);
+
+
+        node.on('close', function () {
             console.log('MagicHome Led Node Closed');
         });
 
         //handle input from either a node red dashboard color picker
         //or a button 
-        node.on('input', function(msg) {
+        node.on('input', function (msg) {
             switch (typeof msg.payload) {
                 // button to switch on and off delivers bool
                 case "boolean":
                     dps = 1;
                     hexval = msg.payload;
                     break;
-                    // color picker delivers string to choose color    
+                // color picker delivers string to choose color    
                 case "string":
-                    if (msg.payload.length != 6) {
-                        console.log("Error: Color String has wrong length(6)");
-                        break;
+                    switch (true) {
+                        case (msg.payload == ""):
+                            return 0;
+                            break;
+                        case (msg.payload.length != 6):
+                            console.log("Error: Color String has wrong length(6)");
+                            return 0;
+                            break;
+                        default:
+                            dps = 5;
+                            hexval = color.set(msg.payload);
                     }
-                    dps = 5;
-                    hexval = color.set(msg.payload);
                     break;
                 default:
                     console.log("Error: Wrong Input Value (must be Bool or String)");
             }
             tuya.set({ dps: dps, set: hexval })
-                .then(status => {})
+                .then(status => {
+                    this.status({ fill: "green", shape: "dot", text: "connected" });
+                })
                 .catch(error => {
                     console.log(error);
-                });
+                    this.status({ fill: "red", shape: "ring", text: "no connection" });
+                })
         });
     }
-    RED.nodes.registerType("magichome-led-node", MagicHomeLedNode);
+    RED.nodes.registerType("magichome-led", MagicHomeLedNode);
 };
